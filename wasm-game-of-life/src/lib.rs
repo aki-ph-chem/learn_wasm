c0dcff483e535f96595fdad2b6bdfa5b33b0791f
aki <aki@mymail.com>
2023-10-05 01:34:14 +0900

    Universeの途中まで実装した

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -12,3 +12,70 @@ pub fn greet(your_name: &str) {
     let fmt = format!("Hello, {}!", your_name);
     alert(&fmt);
 }
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone,Copy,Debug,PartialEq,Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    // セルの座標(row,column)からVec<Cell>におけるindexを計算する
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    // 近接する生きているセルをカウントする
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0; 
+        for delta_row in [self.height -1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+}
+
+#[wasm_bindgen]
+impl Universe {
+    // 次の世代を計算する
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                let next_cell = match (cell, live_neighbors) {
+                    // life gameのルールを反映
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (othterwise, _) => othterwise,
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+        self.cells = next;
+    }
+}
