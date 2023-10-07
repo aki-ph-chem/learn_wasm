13becd2524688c49ae36b67c4ce1c03c6e52aeda
aki <aki@mymail.com>
2023-10-07 15:06:32 +0900

    一応プロファイラを実装したが、数値がNaNになってしまう

--- wasm-game-of-life/www/index.js
+++ wasm-game-of-life/www/index.js
@@ -22,10 +22,11 @@ const ctx = canvas.getContext('2d');
 let animationId = null;
 
 const renderLoop = () => {
-    drawGrid();
-    drawCells();
+    fps.render();
 
     universe.tick();
+    drawGrid();
+    drawCells();
 
     animationId = requestAnimationFrame(renderLoop);
 };
@@ -121,5 +122,44 @@ const drawCells = () => {
 
     ctx.stroke();
 }
+
+const fps = new class {
+    constructor () {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render () {
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / this.delta * 1000;
+
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for(let i = 0; i < this.frames.length; ++i) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        // Render the staties
+        this.fps.textContent = `
+        frames per sedond:
+            latest      = ${Math.round(fps)}
+        avg of last 100 = ${Math.round(mean)}
+        min of last 100 = ${Math.round(min)}
+        max of last 100 = ${Math.round(max)}
+        `.trim(); 
+    }
+};
 
 play();
